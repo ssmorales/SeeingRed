3b05ca22fd5deec792a11856866417f9b28dfd98
ssmorales <ssmorales@mail.csuchico.edu>
2018-10-22 17:28:32 -0700

    red pic

--- main.cpp
+++ main.cpp
@@ -8,6 +8,44 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
+  Bitmap image;
+  image.open("kendall.bmp");
+  cout << argc << endl;
+  cout << argv[0] << endl;
+  vector<vector<Pixel> > imagePixels;
+  int numrows = 0;
+  int numcollumns = 0;
+  Pixel color;
+  if ( argc <= 1 )
+  {
+    cout << "Please spcify one image file!" << endl;
+  }
+  bool validBmp = image.isImage();
+  if ( validBmp == true )
+  {
+    cout << "true" << endl;
+  }
+  else
+  {
+    cout << "false" << endl;
+  }
+  imagePixels = image.toPixelMatrix();
+  for ( int row = 0; row < imagePixels.size(); row++)
+  {
+    numrows = numrows + 1;
   
+  for ( int collumn = 0; collumn < imagePixels[row].size(); collumn++)
+    {
+      
+      
+      color.green = 0;
+      color.blue = 0;
+
+    }
+   }
+   image.fromPixelMatrix( imagePixels );
+   image.save("redness.bmp");
+  cout << imagePixels.size() << endl;
+  cout << imagePixels[0].size() << endl;
   return 0;
 }
